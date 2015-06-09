778abb877e06785b3af6bad5c3616d6eb860512d
David Cairns <drcairns@gmail.com>
2015-06-09 14:08:57 -0700

    Adds Animation.swift

---
+++ DePictLib/DePictLib/Animation.swift
@@ -0,0 +1,16 @@
+//
+//  Animation.swift
+//  DePictLib
+//
+//  Created by David Cairns on 6/8/15.
+//  Copyright (c) 2015 David Cairns. All rights reserved.
+//
+
+import Foundation
+
+public func Animated <T> (numFrames: Int = 24, thing: Double -> T) -> [T] {
+	return Array(0 ... numFrames).map { (idx: Int) -> T in
+		let progress = Double(idx) / Double(numFrames)
+		return thing(progress)
+	}
+}
