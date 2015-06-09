4ea7f816263f5e03fa2fdedb5ecad0c1dc269b3d
David Cairns <drcairns@gmail.com>
2015-06-09 14:12:20 -0700

    Adds the CircleAnimation Example

---
+++ Examples/CircleAnimation.playground/Contents.swift
@@ -0,0 +1,21 @@
+/*:
+## Let’s draw a circle-filling animation!
+
+We can use the `Animated` function to generate a range of images. This makes
+it easy to create pre-baked animations. You can also express animated interface
+elements simply and easily using the same style.
+*/
+
+import DePictLib
+
+let animation: [Image] = Animated(numFrames: 24) { (progress: Double) -> Image in
+	let radial: Double -> Shape = TweenedRadial(centerX: 50, centerY: 50, radius: 50, fromAngle: M_PI_2)
+	return Draw(colorer:
+		Filled(color: Light(Brown), shape: Circle(centerX: 50, Y: 50, radius: 44))
+		+
+		Masked(
+			colorer: Filled(color: Blue, shape: Circle(centerX: 50, Y: 50, radius: 40)),
+			mask: radial(progress)
+		)
+	)
+}
